54e3cfd476f015e68c49f5814540e1f9c162394d
Farid Ahmadian <ahmadian.farid.1988@gmail.com>
2022-04-29 12:28:49 +0200

    chore: replace Github with more wide solution

--- initialer.js
+++ initialer.js
@@ -1,19 +1,22 @@
-var github_username = ""
-var github_repository = ""
+var long_term_saving_location = ""
 var note = ""
 
-chrome.storage.sync.get(['github_repository'], function(result) {
-    github_repository = result.github_repository;
-    document.getElementById("github_repository").value = github_repository;
-});
+function initialer() {
+    chrome.storage.sync.get(['long_term_saving_location'], function(result) {
+        long_term_saving_location = result.long_term_saving_location;
+        if(long_term_saving_location){
+            document.getElementById("long_term_saving_location").value = long_term_saving_location;
+            document.getElementById("popup_note_button").style.visibility = "visible";
+        }
+    });
+    
+    chrome.storage.sync.get(['popup_note_textarea'], function(result) {
+        if(result.popup_note_textarea) {
+            document.getElementById("popup_note_textarea").value = result.popup_note_textarea;
+        }
+    });
+    
+    chrome.action.setBadgeText({text: ''}); 
+}
 
-chrome.storage.sync.get(['github_username'], function(result) {
-    github_username = result.github_username;
-    document.getElementById("github_username").value = github_username;
-});
-
-chrome.storage.sync.get(['popup_note_textarea'], function(result) {
-    document.getElementById("popup_note_textarea").value = result.popup_note_textarea;
-});
-
-chrome.action.setBadgeText({text: ''}); 
+initialer();
--- popup_settings.js
+++ popup_settings.js
@@ -16,15 +16,13 @@ const popup_settings = document.getElementById('popup_settings_icon');
 popup_settings.addEventListener('click', setting);
 
 function saveSetting() {
-    var github_username = document.getElementById("github_username").value
-    chrome.storage.sync.set({"github_username": github_username}, function() {
-        console.log('github_username is set to ' + github_username);
+    var long_term_saving_location = document.getElementById("long_term_saving_location").value
+    chrome.storage.sync.set({"long_term_saving_location": long_term_saving_location}, function() {
+        console.log('long_term_saving_location is set to ' + long_term_saving_location);
     });
-    
-    var github_repository = document.getElementById("github_repository").value
-    chrome.storage.sync.set({"github_repository": github_repository}, function() {
-        console.log('github_repository is set to ' + github_repository);
-    });
+
+    setting();
+    initialer();
 }
 
 const popup_settings_button = document.getElementById('settings_button');
